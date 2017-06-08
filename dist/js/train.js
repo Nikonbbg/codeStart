88dda5d7aa18c0f8bc824cff4c89a135eaaf89ef
Nikonbbg <Nikonbbg@gmail.com>
2017-06-08 17:21:21 +0300

    eq(), first(), last()

--- dist/js/train.js
+++ dist/js/train.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	
-}
+
+	// <!-- 		2 (Обертка для манипуляции с DOM ( выборка, фильтрация через eq(), first(), last() )) -->
+	$( "li" ).first().css( "background-color", "red" );
+	$( "li" ).eq( 2 ).css( "background-color", "#f3f3f3" );
+	$( "li" ).eq( -2 ).css( "background-color", "white" );
+	$( "li" ).last().css( "background-color", "blue" );
+
+});
